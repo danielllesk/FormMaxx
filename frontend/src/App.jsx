3b0eb1856a12be9c9888dece9c4aa8d877aa573d
yenoo <yenooh7099@gmail.com>
2026-01-10 15:28:49 -0500

    resolved merge conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,16 @@ import './App.css'
 import HumanBody from './components/human-body.jsx'
 import MuscleSelectButton from './components/muscle-select-button.jsx'
 import Exercise from './components/exercise.jsx'
+import ExersizeCard from './components/exersize-card.jsx'
+import BarbellCurlsImage from './assets/BarbellCurls.png'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
+    <ExersizeCard exercise="Barbell Curl" description="A classic biceps isolation exercise focusing on elbow flexion." exerciseImage={BarbellCurlsImage} />
+    <MuscleSelectButton label="Muscle Select Button" onClick={() => console.log('clicked')} />
     <BrowserRouter>
       <Routes> 
         <Route path="/" element={<HumanBody />} />
