7cb909023a34877f177fb85de84258c7af2ddde9
Sahil Gokhale <sahil.gokhale2279@gmail.com>
2026-01-10 16:00:47 -0500

    humanBody changes

--- frontend/src/components/human-body.jsx
+++ frontend/src/components/human-body.jsx
@@ -1,4 +1,5 @@
 import React from 'react'; 
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import humanAnatomy from '../assets/human_anatomy.png';
 import MuscleSelectButton from './muscle-select-button.jsx'
@@ -6,29 +7,36 @@ import './human-body.css';
 
 export default function HumanBody() {
     const navigate  = useNavigate(); 
-    const humanMuscles = [ { label: 'Biceps', onClick: handleClick, description: 'A classic biceps isolation exercise focusing on elbow flexion.', exerciseImage: 'barbell-curl.png'},
-                            { label: 'Triceps', onClick: handleClick },
-                            { label: 'Shoulders', onClick: handleClick },
-                            { label: 'Chest', onClick: handleClick  },
-                            {label: 'Abdomen', onClick: handleClick},
-                            {label: 'Quandriceps', onClick: handleClick},
-                            {label: 'Hamstrings', onClick: handleClick},
-                            { label: 'Calves', onClick: handleClick},
-                            {label: 'Glutes', onClick: handleClick},
-                            { label: 'Forearms', onClick: handleClick },
-                            { label: 'Lats', onClick: handleClick },
-                            { label: 'Traps', onClick: handleClick },
-                            { label: 'Upper Back', onClick: handleClick },
-                            ]
+    const [setLabel, setLabelName] = useState('');
+    const humanMuscles = [ { label: 'Biceps', onClick: handleClick, className:'Biceps', onMouseEnter: () => setLabelName('Biceps'), onMouseLeave: () => setLabelName('') },
+                            { label: 'Triceps', onClick: handleClick, className:'Triceps', onMouseEnter: () => setLabelName('Triceps'), onMouseLeave: () => setLabelName('') },
+                            { label: 'Shoulders', onClick: handleClick, className:'Shoulders', onMouseEnter: () => setLabelName('Shoulders'), onMouseLeave: () => setLabelName('') },
+                            { label: 'Chest', onClick: handleClick, className:'Chest', onMouseEnter: () => setLabelName('Chest'), onMouseLeave: () => setLabelName('') },
+                            {label: 'Abdomen', onClick: handleClick, className:'Abdomen', onMouseEnter: () => setLabelName('Abdomen'), onMouseLeave: () => setLabelName('') },
+                            {label: 'Quadriceps', onClick: handleClick, className:'Quadriceps', onMouseEnter: () => setLabelName('Quadriceps'), onMouseLeave: () => setLabelName('') },
+                            {label: 'Hamstrings', onClick: handleClick, className:'Hamstrings', onMouseEnter: () => setLabelName('Hamstrings'), onMouseLeave: () => setLabelName('') },
+                            { label: 'Calves', onClick: handleClick, className:'Calves', onMouseEnter: () => setLabelName('Calves'), onMouseLeave: () => setLabelName('') },
+                            {label: 'Glutes', onClick: handleClick, className:'Glutes', onMouseEnter: () => setLabelName('Glutes'), onMouseLeave: () => setLabelName('') },
+                            { label: 'Forearms', onClick: handleClick, className:'Forearms', onMouseEnter: () => setLabelName('Forearms'), onMouseLeave: () => setLabelName('') },
+                            { label: 'Lats', onClick: handleClick, className:'Lats', onMouseEnter: () => setLabelName('Lats'), onMouseLeave: () => setLabelName('') },
+                            { label: 'Traps', onClick: handleClick, className:'Traps', onMouseEnter: () => setLabelName('Traps'), onMouseLeave: () => setLabelName('') },
+                            { label: 'Upper Back', onClick: handleClick, className:'UpperBack', onMouseEnter: () => setLabelName('Upper Back'), onMouseLeave: () => setLabelName('') },
+    ]
     function handleClick(muscle) {
         navigate("/exercise", { state: { muscle: muscle.label } })
     }
     return (
-        <div className="human-body-container"> 
-            <img className="Human-img" src={humanAnatomy} alt="Human Body" />
-            {humanMuscles.map((muscle, index) => (
-                <MuscleSelectButton key={index} label={muscle.label} onClick={() => handleClick(muscle)} className={muscle.className}/>
-            ))}
-        </div>
+        <div>
+        <h2>Select a Muscle to Target: {setLabel}</h2>
+            <div className="human-body-container"> 
+                <img className="Human-img" src={humanAnatomy} alt="Human Body" />
+                    {humanMuscles.map((muscle, index) => (
+                    <MuscleSelectButton key={index} label={muscle.label} onClick={() => handleClick(muscle)} className={muscle.className}
+                    onMouseEnter={() => setLabelName(muscle.label)}
+                    onMouseLeave={() => setLabelName('')}
+                    />
+                ))}
+                </div>
+            </div>
     );
 }
--- frontend/src/components/muscle-select-button.jsx
+++ frontend/src/components/muscle-select-button.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const MuscleSelectButton = ({label, onClick, className}) => {
+const MuscleSelectButton = ({label, onClick, className, onMouseEnter, onMouseLeave}) => {
     return (
-        <button onClick={onClick} className={className}>
+        <button onClick={onClick} className={className} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
             {label}
         </button>
     );
