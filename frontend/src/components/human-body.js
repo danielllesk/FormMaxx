e793a9b5b2ee57f33ce3427f7b9f5246e48f6eda
Sahil Gokhale <sahil.gokhale2279@gmail.com>
2026-01-10 12:07:01 -0500

    components

---
+++ frontend/src/components/human-body.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+export default function HumanBody() {
+    function handleClick() {
+        
+    }
+    return (
+        <div> 
+            <p> this will be a human </p> 
+        </div>
+    )
+}
